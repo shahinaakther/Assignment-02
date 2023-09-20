26043066a45520d4e0e76fae3c583cf2b18647b7
shahinaakther <shahinamahrus@gmail.com>
2023-09-20 23:22:13 +0600

    2nd commit

--- Break_Condition.php
+++ Break_Condition.php
@@ -1,7 +1,7 @@
 <?php
 
-for ($a = 0; $a < 10; $a++) {
-  if ($a == 10) {
+for ($a = 0; $a < 200; $a++) {
+  if ($a == 101) {
     break;
   }
   echo "The number is: $a <br>";
--- Fibonacci_Series.php
+++ Fibonacci_Series.php
@@ -1,21 +1,12 @@
 <?php
-function evenNumberDoWhile($startPoint, $endPoint, $step)
-{
-   $i = $startPoint;
-    do {
-        if ($i % 2 != 0) {
-
-            echo $i+1;
-            for ($j = 1; $j <= $i - 1; $j++) {
-            }
-            if ($j == $endPoint) {
-                echo ".";
-            } else {
-                echo ", ";
-            }
-        }
-        $i = $i + $step;
-    } while ($i <= $endPoint);
+function number(){
+for ($x = 0; $x<=10; $x++){
+    echo "the number is: $x <br>";
 }
-
-    echo "\n";
+}
+number();
+echo "<br>";
+number();
+echo "<br>";
+number();
+echo "<br>";
